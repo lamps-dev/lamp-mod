43f9539fb091c1a99fdecc3f50097001851a3515
ilovelampadaire <ilovelampadaire@gmail.com>
2025-02-09 18:20:57 +0100

    V0, Initial Project Release

---
+++ src/main/java/net/lamp/lampmod/LampMod.java
@@ -0,0 +1,16 @@
+package net.lamp.lampmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LampMod implements ModInitializer {
+	public static final String MOD_ID = "lampmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/lamp/lampmod/LampModClient.java
@@ -0,0 +1,10 @@
+package net.lamp.lampmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LampModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
